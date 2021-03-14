4f95cc1088e846341bb055cf8273728df48947ad
Bartosz Warzecha <bartosz.warzecha@microsoft.wsei.edu.pl>
2021-03-14 17:47:39 +0100

    Spersonalizowane Powitanie

---
+++ Wyklad1/Wyklad1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Wyklad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj!");
+
+            Console.Write("Podaj imie: ");
+            string imie = Console.ReadLine();
+
+            Console.Write("Podaj nazwisko: ");
+            string nazwisko = Console.ReadLine();
+
+            //Console.WriteLine("Witaj" + imie + nazwisko + "!");
+            //Console.WriteLine("Witaj {0} {1}!", nazwisko, imie);
+            Console.WriteLine($"Witaj {imie} {nazwisko}!");
+        }
+    }
+}
